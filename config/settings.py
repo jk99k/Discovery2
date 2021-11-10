fb3ee71c16dcb5efc977240e32b944d2304e37a5
popohato <64397377+popohato@users.noreply.github.com>
2021-11-10 19:37:36 +0900

    second commit

--- config/settings.py
+++ config/settings.py
@@ -42,6 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'rest_framework',
+    'corsheaders',
+
 
     'books',
 ]
@@ -49,12 +51,17 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'whitenoise.middleware.WhiteNoiseMiddleware',
+]
+
+CORS_ORIGIN_WHITELIST = [
+    '*'
 ]
 
 ROOT_URLCONF = 'config.urls'
